71bd41a98fd4681f7af3a7643d54621a7b8cb695
Caroline Leite <87489383+carolineleite@users.noreply.github.com>
2021-09-23 21:41:53 -0300

    Atualização

---
+++ funcoes.py
@@ -0,0 +1,29 @@
+lista = [3, 2, 1]
+
+print(sum(lista))
+
+
+def somar():
+    a = float(input('Digite um valor: '))
+    b = float(input('Digite outro valor: '))
+
+    soma = a + b
+
+    print(soma)
+
+
+somar()
+
+
+def subtrair(a, b):
+
+    subtrai = a - b
+
+    print(subtrai)
+
+
+valor1 = float(input('Digite um valor: '))
+valor2 = float(input('Digite outro valor: '))
+subtrair(valor1, valor2)
+
+
