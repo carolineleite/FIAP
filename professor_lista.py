e500eb4890f45b499e89eef9a2402f3373bd9aed
Caroline Leite <87489383+carolineleite@users.noreply.github.com>
2021-09-23 21:21:35 -0300

    Atualização

---
+++ professor_lista.py
@@ -0,0 +1,16 @@
+notas = []
+opcao = -1
+
+while opcao != 4:
+    print('1 - Informar notas \n'
+          '2 - Exibir notas \n'
+          '3 - Calcular média\n'
+          '4 - Sair')
+    opcao = int(input('Escolha sua opção: '))
+    if opcao == 1:
+        notas.append(float(input('Digite a nota: ')))
+    elif opcao == 2:
+        for x in notas:
+            print(x)
+    elif opcao == 3:
+        print(sum(notas)/len(notas))
