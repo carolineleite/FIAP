e708f1cb52883bf41c03dd96dded17b2bd713aa5
Caroline Leite <87489383+carolineleite@users.noreply.github.com>
2021-09-26 21:11:10 -0300

    Atualização

--- RM92033_JSON.py
+++ RM92033_JSON.py
@@ -5,21 +5,29 @@
 import json
 
 
-def ler_json() -> object:
+def ler_json():
     with open("star_wars.json", "r", encoding='utf-8') as arquivo:
         return json.load(arquivo)
-
 
 data = ler_json()
 
 nomes = [d['name'] for d in data]
 generos = [d['gender'] for d in data]
 
-for nome in nomes:
-    print(f'O nome do personagem é: {nome}')
+#for nome in nomes:
+#    print(f'O nome do personagem é: {nome}')
 
-for genero in generos:
-    print(f'O gênero é: {gerero}')
+male = generos.count('male')
+print(f'A quantidade de personagens homens é: {male}')
 
+female = generos.count('female')
+print(f'A quantidade de personagens mulheres é: {female}')
 
+nao_se_aplica = generos.count("n/a")
+print(f'A quantidade de personagens com gênero que não se aplica é: {nao_se_aplica}')
 
+hermaphrodite = generos.count('hermaphrodite')
+print(f'A quantidade de personagens com gênero hermafrodita é: {hermaphrodite}')
+
+none = generos.count('none')
+print(f'A quantidade de personagens sem gênero é: {none}')
