a46fc5d4b6388d97f12f5d182aee59160af75044
Caroline Leite <87489383+carolineleite@users.noreply.github.com>
2021-09-28 18:57:43 -0300

    Atualização

--- RM92033_EX01.py
+++ RM92033_EX01.py
@@ -3,13 +3,16 @@ alimento = ''
 sair = ''
 qtd_calorias = 0
 qtd_alimento_soma = 0
+qtd_calorias_total = 0
 
-while sair != 'Sim':
+print('Seja bem-vindo a Calculadora de Calorias')
+
+while sair != 'SIM':
     alimento = input('Qual alimento foi consumido? ')
     qtd_alimento = input('Qual a quantidade ingerida desse alimento? ')
-    resposta = input('Qual a quantidade de calorias desse alimento? ')
-    sair = input('Você deseja sair? Sim ou Não? ')
-    qtd_calorias = int(qtd_calorias) + int(resposta) * int(qtd_alimento)
+    qtd_calorias = input('Qual a quantidade de calorias desse alimento? ')
+    sair = input('Você deseja sair? Sim ou Não? ').upper()
+    qtd_calorias_total = int(qtd_calorias_total) + int(qtd_calorias) * int(qtd_alimento)
     qtd_alimento_soma = int(qtd_alimento_soma) + int(qtd_alimento)
 
-print(f'Foram ingeridas {qtd_calorias} calorias e {qtd_alimento_soma} alimentos')
+print(f'Foram ingeridas {qtd_calorias_total} calorias e {qtd_alimento_soma} alimentos')
--- teste.py
+++ teste.py
@@ -1,16 +1,26 @@
-alimento_ingerido = ""
-qtd_alimentos = 0
-calorias_alimento = 0
-qtd_calorias_ingeridas = 0
-qtd_alimentos_ingeridos = 0
-adicionar = ""
+#Este programa calculará o total de calorias ingeridas pelo usuário no presente dia .
+#Definindo as variáveis
+nome_alimento = ""
+qtde_alimento = 0
+qtde_caloria = 0
+soma_alimento = 0
+soma_caloria = 0
+sair = ""
+#Introdução do programa
+nome = input("Digite seu nome: ")
+senha = input("Por favor, digite a sua senha: ")
+print("Olá {}! Vamos calcular a quantidade de caloridas ingeridas hoje!".format(nome))
 
-while adicionar != "NÃO":
-    alimento_ingerido = input("Qual alimento voce consumiu? ")
-    qtd_alimentos = int(input("Quantidade consumida? "))
-    calorias_alimento = int(input("Quantas calorias tem esse alimento? "))
-    qtd_alimentos_ingeridos = int(qtd_alimentos_ingeridos) + int(qtd_alimentos)
-    qtd_calorias_ingeridas = int(qtd_calorias_ingeridas) + int(qtd_alimentos) * int(calorias_alimento)
-    adicionar = input("Gostaria de adicionar mais alimentos? 'Sim' ou 'Não' ").upper()
+#Iniciando processo de loop
+while sair != "2":
+    nome_alimento = input("Digite o nome do alimento: ")
+    qtde_alimento = int(input("Insira a quantidade do alimento: "))
+    qtde_caloria = int(input("Insira a quantidade de caloria do alimento: "))
+    sair = input("Você deseja inserir mais algum alimento, {}? 1-Sim ou 2-Não?".format(nome))
 
-print("Voce consumiu {} alimentos com um total de {} Kcal.".format(qtd_alimentos_ingeridos, qtd_calorias_ingeridas))
+#Cálculo realizado para a soma da quantidade de calorias ingeridas.
+soma_alimento = soma_alimento + qtde_alimento
+soma_caloria = soma_caloria + qtde_caloria * qtde_alimento
+
+#Caso o usuário não queria continuar, insira a mensagem abaixo
+print ("Foram consumidas {} calorias neste dia, {}.".format(soma_caloria, nome))
